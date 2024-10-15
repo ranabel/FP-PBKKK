b5b4b7ee649a823ef99fb9baea7ddd220754f0f8
ranabel <nabilahatikarahma26@gmail.com>
2024-10-15 10:27:42 +0700

    update2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,39 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class ProductController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $products = Product::paginate(12);
 
         return view('products.index', compact('products'));
     }
 
-    public function create() {
-
+    public function create()
+    {
         return view('products.create');
     }
 
-    use ValidatesRequests;
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
+        $this->validate($request, [
             'nama' => 'required',
             'harga' => 'required|numeric',
-            'deskripsi' => 'nullable', // Allow nullable for optional description
-            'foto' => 'required|image|mimes:png,jpg,jpeg',
+            'foto' => 'required|image|mimes:jpeg,png,jpg'
         ]);
 
         $foto = $request->file('foto');
-        $uniqueFilename = $foto->hashName() . time() . '.' . $foto->extension(); // Add timestamp for better uniqueness
-        $foto->storeAs('public', $uniqueFilename);
+        $foto->storeAs('public', $foto->hashName());
 
-        $validatedData['foto'] = $uniqueFilename;
-
-        Product::create($validatedData);
+        Product::create([
+            'nama' => $request->nama,
+            'harga' => $request->harga,
+            'deskripsi' => $request->deskripsi,
+            'foto' => $foto->hashName()
+        ]);
 
         return redirect()->route('products.index')->with('success', 'Add Product Success');
     }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,15 +1,16 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 px-2">
 
-        @if(session()->has('success'))
+        @if (session()->has('success'))
             <x-alert message="{{ session('success') }}" />
         @endif
-        
+
         <div class="flex mt-6 items-center justify-between">
             <h2 class="font-semibold text-xl">List Products</h2>
             <a href="{{ route('products.create') }}">
                 <button class="bg-gray-100 px-10 py-2 rounded-md font-semibold">Add</button>
             </a>
+
         </div>
 
         <div class="grid md:grid-cols-3 grid-cols-1 mt-4 gap-6">
@@ -18,13 +19,13 @@
                     <img src="{{ url('storage/' . $product->foto) }}" />
                     <div class="my-2">
                         <p class="text-xl font-light">{{ $product->nama }}</p>
-                        <p class="font-semibold text-gray-400">RP. {{ number_format ($product->harga) }}</p>
+                        <p class="font-semibold text-gray-400">Rp. {{ number_format($product->harga) }}</p>
                     </div>
-
                     <button class="bg-gray-100 px-10 py-2 w-full rounded-md font-semibold">Edit</button>
                 </div>
             @endforeach
         </div>
+
 
         <div class="mt-4">
             {{ $products->links() }}
