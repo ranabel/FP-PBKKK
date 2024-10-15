c9a33814419eef4a578d859eada76aa1b5b9e2a7
ranabel <nabilahatikarahma26@gmail.com>
2024-10-15 13:02:32 +0700

    update3

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -16,12 +16,15 @@
         <div class="grid md:grid-cols-3 grid-cols-1 mt-4 gap-6">
             @foreach ($products as $product)
                 <div>
-                    <img src="{{ url('storage/' . $product->foto) }}" />
+                    <img src="{{ url('storage/' . $product->foto) }}" class="h-96 w-full object-cover" />
                     <div class="my-2">
                         <p class="text-xl font-light">{{ $product->nama }}</p>
                         <p class="font-semibold text-gray-400">Rp. {{ number_format($product->harga) }}</p>
                     </div>
-                    <button class="bg-gray-100 px-10 py-2 w-full rounded-md font-semibold">Edit</button>
+                    <a href="{{ route('products.edit', $product) }}">
+                        <button class="bg-gray-100 px-10 py-2 w-full rounded-md font-semibold">Edit</button>
+                    </a>
+
                 </div>
             @endforeach
         </div>
